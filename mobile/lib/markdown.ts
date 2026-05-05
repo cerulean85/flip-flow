56797495c35973e84ba8d983c2a51376a4d65a4a
cerulean85 <zhwan85@gmail.com>
2026-05-05 15:20:34 +0900

    update: 마크다운 렌더링 수정 2

--- mobile/lib/markdown.ts
+++ mobile/lib/markdown.ts
@@ -1,3 +1,5 @@
 export function normalizeEssayMarkdown(markdown: string) {
-  return markdown.replace(/([^\s*])\*\*((?:'|’)[^*\n]*?)\*\*/g, "$1&ZeroWidthSpace;**$2**")
+  return markdown
+    .replace(/([^\s*])\*\*((?:'|’)[^*\n]*?)\*\*/g, "$1&ZeroWidthSpace;**$2**")
+    .replace(/\*\*((?:['’"][^*\n]*?['’"]))\*\*(?=[\p{L}\p{N}_])/gu, "**$1**&ZeroWidthSpace;")
 }
